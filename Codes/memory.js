b3f39867789478506bb948f959801daa2300802e
Abhishek Nandeppagol <117501828+Abhishekmn1999@users.noreply.github.com>
2024-06-01 17:26:56 +0000

    Heap

---
+++ Codes/memory.js
@@ -0,0 +1,11 @@
+let userOne={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let usertwo = userOne
+
+usertwo.email="usertwo@gmail.com";
+
+console.log(userOne.email);
+console.log(usertwo.email);
