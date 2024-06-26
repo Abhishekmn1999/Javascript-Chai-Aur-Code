f621feeb1177576600f8a2c8cb7f8bbc7e2093d4
Abhishek Nandeppagol <117501828+Abhishekmn1999@users.noreply.github.com>
2024-06-26 13:49:44 +0000

    Variables

---
+++ Codes/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountName = "Abhi"
+ accountCity = "Belagavi"
+let accountState
+
+// const accountId = 54321 This is not allowed
+
+console.table([accountId,accountName,accountCity,accountState]);
+
+console.log(typeof(accountCity));
